20352e6055d29a6830771e203991f6982eaf9596
preetishashrestha <preetishashrestha@gmail.com>
2026-05-18 23:02:59 +0545

    fixed register.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('register/',register,name='register'),
-    path('login',log_in,name='log_in')
+    path('login/',log_in,name='log_in')
 ]
