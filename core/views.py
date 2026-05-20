a947f3e8e4791491d9d8d010037d5adfc7cddf66
preetishashrestha <preetishashrestha@gmail.com>
2026-05-21 00:18:38 +0545

    Added Category and subcategory count ans ckeditor for the description part

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import OfferProduct, Category,Product
+from .models import OfferProduct, Category,Product, SubCategory
+from django.db.models import Count, Prefetch
 
 # Create your views here.
 def index(request):
     offer=OfferProduct.objects.filter(is_available=True)
-    category=Category.objects.all()
+    category=Category.objects.annotate(count_sub=Count("subcategory")).prefetch_related(Prefetch('subcategory_set',queryset=\
+    SubCategory.objects.annotate(product_count=Count('product'))))
     product=Product.objects.all()
     context={
         "offer":offer,
