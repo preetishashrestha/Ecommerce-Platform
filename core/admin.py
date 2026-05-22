fb8702397b2499744a372b5d49ba7a73fde3ae5f
preetishashrestha <preetishashrestha@gmail.com>
2026-05-22 23:37:29 +0545

    fixed login css

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,7 @@ from .models import *
 admin.site.register(OfferProduct)
 admin.site.register(Category)
 admin.site.register(SubCategory)
+admin.site.register(Brand)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display=[
