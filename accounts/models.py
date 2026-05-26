db7173d49af952845337cf030b7cadde17410a7d
preetishashrestha <preetishashrestha@gmail.com>
2026-05-26 21:49:46 +0545

    added the profile

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,9 @@ class CustomUser(AbstractUser):
     street_address=models.CharField(max_length=200)
 
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    profile_picture=models.ImageField(upload_to='images')
+    bio=models.TextField()
+    dob=models.DateField(null=True)
+    created_at=models.DateField(auto_now=True)
