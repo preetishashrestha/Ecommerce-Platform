f26620875691ad373f35947dba1516e535a9d8f9
preetishashrestha <preetishashrestha@gmail.com>
2026-06-01 22:18:22 +0545

    made the review part dynamic

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django_ckeditor_5.fields import CKEditor5Field
+from accounts.models import CustomUser
 
 class OfferProduct(models.Model):
     title=models.CharField(max_length=200)
@@ -61,3 +62,9 @@ class ProductImage(models.Model):
     def __str__(self):
         return self.product.name
     
+class Review(models.Model):
+    user=models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    rating=models.PositiveSmallIntegerField()
+    feedback=models.TextField()
+    created_at=models.DateField(auto_now=True)
